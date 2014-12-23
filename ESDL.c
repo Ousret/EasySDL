3a85297c67ce6607c0441da8b9ca4d4304529697
TAHRI Ahmed <nyuubi.10@gmail.com>
2014-12-23 21:02:45 +0100

    Minor fix for sprite blit.

--- ESDL.c
+++ ESDL.c
@@ -935,8 +935,8 @@ void SDL_BlitObjs(t_window * window) {
 		if (!(window->windowSprite[i].hide)) {
 			
 			//Animation .. Orientation
-			spritePos.x = ((window->windowSprite[i].animation % ((window->windowSprite[i].width) / (window->windowSprite[i].sp_width))) * (window->windowSprite[i].sp_width));
-    		spritePos.y = ((window->windowSprite[i].position % ((window->windowSprite[i].height) / (window->windowSprite[i].sp_height))) * (window->windowSprite[i].sp_height));
+			spritePos.x = ((window->windowSprite[i].animation % ((window->windowSprite[i].width) / (window->windowSprite[i].sp_width))) * (window->windowSprite[i].sp_width))-(window->windowSprite[i].sp_width);
+    		spritePos.y = ((window->windowSprite[i].position % ((window->windowSprite[i].height) / (window->windowSprite[i].sp_height))) * (window->windowSprite[i].sp_height))-(window->windowSprite[i].sp_height);
     		spritePos.w = window->windowSprite[i].sp_width;
     		spritePos.h = window->windowSprite[i].sp_height;
     		

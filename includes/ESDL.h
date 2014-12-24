aaf8be8ca2420bfc4a7b138b02da7129feb7157a
TAHRI Ahmed <nyuubi.10@gmail.com>
2014-12-25 00:16:59 +0100

    ESDL 0.5 header

--- includes/ESDL.h
+++ includes/ESDL.h
@@ -2,18 +2,23 @@
  * \file ESDL.h
  * \brief EasySDL header
  * \author TAHRI Ahmed, SIMON Jérémy
- * \version 0.1
- * \date 08/12/2014
+ * \version 0.4.5
+ * \date 22/12/2014
  *
  * Lib for creating SDL program easily.
  *
  */
  
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+ 
 #define N 10
 #define M 50
 
 typedef enum {
-
+	
 	NUMERIC,
 	VARCHAR,
 	NUMCHAR,
@@ -21,17 +26,15 @@ typedef enum {
 	
 } t_typeForm;
 
-GPU_Image *BTNOVER = NULL, *BTNNOTOVER = NULL, *FORM = NULL;
-Mix_Chunk *SELECT = NULL, *ENTER = NULL;
-
 /**
  * \struct t_object
  * \brief Represent object, could be button or form input to be filled.
  *
  * t_object contain info of one object (button or form input)
  */
-typedef struct {
 
+typedef struct {
+	
 	int type;
 	int x;
 	int y;
@@ -41,8 +44,7 @@ typedef struct {
 	char * dest; //Only for form obj
 	t_typeForm typeForm; //Only for form obj
 	int MouseOver; // 1 = Mouse is over, 0 = Not over..
-	GPU_Image * GPU_buffer_title;
-	GPU_Image * GPU_buffer_content;
+	SDL_Surface *buffer_title, *buffer_content;
 	
 } t_object;
 
@@ -58,7 +60,7 @@ typedef struct {
 	char * content;
 	int x;
 	int y;
-	GPU_Image * GPU_buffer;
+	SDL_Surface *buffer;
 	
 } t_text;
 
@@ -69,15 +71,33 @@ typedef struct {
  * t_texture contain info of one image, his size in pixels and his pos
  */
 typedef struct {
-
-	char * file;
+	
 	int x;
 	int y;
 	int height;
 	int width;
-	GPU_Image * GPU_buffer;
+	SDL_Surface *buffer;
 
 } t_texture;
+
+typedef struct {
+	
+	char file[150];
+	Mix_Chunk * MIX_BUF;
+	
+} t_audio;
+
+typedef struct {
+
+	SDL_Surface *buffer;
+	SDL_Color transparancy;
+	int height, width;
+	int sp_height, sp_width;
+	int x, y;
+	int position, animation;
+	int hide;
+
+} t_sprite;
 
 /**
  * \struct t_window
@@ -88,7 +108,8 @@ typedef struct {
 typedef struct {
 
 	char * title;
-	GPU_Image * windowSnap;
+	SDL_Surface * windowSurface;
+	SDL_Texture * windowRd;
 	
 	t_object * windowObj;
 	int nbObj;
@@ -98,6 +119,9 @@ typedef struct {
 	
 	t_texture * windowImg;
 	int nbImg;
+	
+	t_sprite * windowSprite;
+	int nbSprite;
 	
 	int x, y;
 	int height, width;
@@ -112,7 +136,7 @@ typedef struct {
  */
 typedef struct
 {
-	char key[256];
+	char key[SDLK_SLEEP];
 	int mousex,mousey;
 	int mousexrel,mouseyrel;
 	char mousebuttons[8];
@@ -121,11 +145,12 @@ typedef struct
 } Input;
 
 /**
- * \fn SDL_init(int x, int y, char titre[100], int ttf_support, char police_name[100], int police_size, int audio_support)
+ * \fn SDL_init(int x, int y, int fullscreen, char titre[100], int ttf_support, char police_name[100], int police_size, int audio_support)
  * \brief Init SDL with your preferences
  *
  * \param x Size in pixel
  * \param y Size in pixel
+ * \param fullscreen Enable fullscreen or not
  * \param titre Title of the SDL handle
  * \param ttf_support Decide if SDL_ttf will load up
  * \param police_name Police filename stored in ressources/ttf/
@@ -133,7 +158,7 @@ typedef struct
  * \param audio_support Decide if SDL_mixer will load up
  * \return No return value, if it fail, it'll close the program with err message.
  */
-void SDL_init(int x, int y, char titre[100], int ttf_support, char police_name[100], int police_size, int audio_support);
+void SDL_init(int x, int y, int fullscreen, char * titre, char * icon_name, int ttf_support, char * police_name, int police_size, int audio_support);
 
 /**
  * \fn int SDL_generateMenu(int nb_entree, char sommaire[N][M])
@@ -143,7 +168,8 @@ void SDL_init(int x, int y, char titre[100], int ttf_support, char police_name[1
  * \param sommaire[N][M] Contain text caption
  * \return Return the value of choise (0 - (nb_entree-1))
  */
-int SDL_generateMenu(int nb_entree, char sommaire[N][M]);
+int SDL_generateMenu(int nb_entree, char captions[][M]);
+
 /**
  * \fn int SDL_IsMouseOver(t_window * window, int hauteur, int largeur, int x, int y)
  * \brief Check if mouse is over area definied in pixel
@@ -157,27 +183,85 @@ int SDL_generateMenu(int nb_entree, char sommaire[N][M]);
  */
 int SDL_IsMouseOver(t_window * window, int hauteur, int largeur, int x, int y);
 
+/**
+ * \fn void SDL_playwav(char * wavfile, int waitEnd, int *channel);
+ * \brief Play wav file
+ *
+ * \param wavfile Name of the wav file to play (must be in ressources/snd/)
+ * \param waitEnd Stop the current thread until playing is over
+ * \param channel Channel used by SDL_Mixer to play WAV. (PTR)
+ * \return void
+ */
 void SDL_playwav(char * wavfile, int waitEnd, int *channel);
 
-void SDL_Splash(char img[100], int attente);
-
+/**
+ * \fn int SDL_IsMouseOverObj(t_window * window);
+ * \brief Check if mouse is over an object and return his obj id
+ *
+ * \param window Window name (created with newWindow())
+ * \return Obj id or -1
+ */
 int SDL_IsMouseOverObj(t_window * window);
+
+/**
+ * \fn void SDL_BlitObjs(t_window * window);
+ * \brief Print on the screen specific window data
+ *
+ * \param window Window name (created with newWindow())
+ * \return void
+ */
 void SDL_BlitObjs(t_window * window);
+
+/**
+ * \fn void SDL_UpdateEvents(Input* in);
+ * \brief Get at t instant event from input
+ *
+ * \param in Input struct that contains key mapping with 0/1 for pressed or not
+ * \return void
+ */
 void SDL_UpdateEvents(Input* in);
-int SDL_MessageBox();
+
+/**
+ * \fn int SDL_CaptureForm(t_window * window, int obj);
+ * \brief Copy buffer (key.unicode) to form obj if mouse is over his area
+ *
+ * \param window Window that contain form obj
+ * \param obj ObjID (form) dest for buffer cpy
+ * \return void
+ */
 int SDL_CaptureForm(t_window * window, int obj);
 
-void SDL_newObj(t_window * window, int * id, int type, char title[50], char * dest, t_typeForm typeForm, int x, int y, int height, int width);
-void SDL_modObj(t_window * window, int obj, int type, char title[50], char * dest, t_typeForm typeForm, int x, int y, int height, int width);
-void SDL_delObj(t_window * window, int obj);
+/**
+ * \fn void SDL_newObj(t_window * window, int * id, int type, char title[50], char * dest, t_typeForm typeForm, int x, int y, int height, int width);
+ * \brief Create a new object into *window data buffer
+ *
+ * \param window Window target
+ * \param id Get the ID of this new obj if created
+ * \param type 0 = Button, 1 = Form to fill
+ * \param title Caption of this new object
+ * \param dest Addr of char * var if type = 1
+ * \param typeForm If type=1, you need to precise the mask
+ * \param x Pos X, relative to window
+ * \param y Pos Y, relative to window
+ * \param height Height in pixel
+ * \param width Width in pixel
+ * \return void
+ */
+int SDL_newObj(t_window * window, int * id, int type, char title[50], char * dest, t_typeForm typeForm, int x, int y, int height, int width);
+int SDL_modObj(t_window * window, int obj, int type, char title[50], char * dest, t_typeForm typeForm, int x, int y, int height, int width);
+int SDL_delObj(t_window * window, int obj);
 
-void SDL_newText(t_window * window, int * id, char * content, SDL_Color couleur, int x, int y);
-void SDL_modText(t_window * window, int idtext, char * content, SDL_Color couleur, int x, int y);
-void SDL_delText(t_window * window, int idtext);
+int SDL_newText(t_window * window, int * id, char * content, SDL_Color couleur, int x, int y);
+int SDL_modText(t_window * window, int idtext, char * content, SDL_Color couleur, int x, int y);
+int SDL_delText(t_window * window, int idtext);
 
-void SDL_newTexture(t_window * window, int * id, char * file, int x, int y, int height, int width);
-void SDL_modTexture(t_window * window, int idimg, char * file, int x, int y, int height, int width);
-void SDL_delTexture(t_window * window, int idimg);
+int SDL_newTexture(t_window * window, int * id, char * file, int x, int y, int height, int width);
+int SDL_modTexture(t_window * window, int idimg, int x, int y, int height, int width);
+int SDL_delTexture(t_window * window, int idimg);
+
+int SDL_newSprite(t_window *window, char * filename, SDL_Color transparancy, int height, int width,int sp_height, int sp_width, int x, int y, int position, int animation, int hide);
+int SDL_modSprite(t_window *window, int idSprite, int x, int y, int position, int animation, int hide);
+int SDL_delSprite(t_window *window, int idSprite);
 
 int SDL_generate(t_window * window);
 
@@ -187,14 +271,25 @@ int SDL_nbTexture(t_window * window);
 int SDL_windowEmpty(t_window * window);
 
 t_window * SDL_newWindow(char * title, int x, int y, int height, int width);
+
 void SDL_freeWindow(t_window * window);
 
-GPU_Image* SDL_convertTexture(SDL_Surface* surface);
 void SDL_loadRessources();
+void SDL_unload();
+int SDL_isKeyPressed(int key);
+void SDL_unloadRessources();
+void SDL_preloadwav(char * wavfile);
 
-/** \brief Calque principal (sur lequel tout est superposé) */
-//extern SDL_Surface *screen;
-extern GPU_Target* screen;
+extern SDL_Surface *BTN_NOTOVER, *BTN_OVER, *FORM;
+extern Mix_Chunk *SELECT, *ENTER;
+
+t_audio MIXTEMP[50];
+extern int nbSnd;
+
+extern SDL_Window *screen;
+extern SDL_Renderer *renderer;
+
+extern Uint32 *myPixels;
 
 /** \brief Couleurs RGB */
 extern SDL_Color colorRed;
@@ -203,22 +298,11 @@ extern SDL_Color colorWhite;
 /** \brief Couleurs RGB */
 extern SDL_Color colorBlack;
 
+extern SDL_Color colorGreenLight;
+
 /** \brief Contient la suite des événements capturés par la SDL (Clavier+Souris) */
 extern SDL_Event GlobalEvent;
 /* Fin pointeur SDL */
-
-/* Variable pour le mixage */
-extern int channel;	
-/** \brief Mixage du son */
-extern Mix_Chunk *sound;
-
-extern Mix_Chunk *music;
-/** \brief Mixage du son */
-extern int channel_music;
-
-extern int channel_effect;	
-/** \brief Mixage du son */
-extern Mix_Chunk *effect;
 
 //SDL_mutex *MutexEcran = NULL; Inutile pour l'instant.. Faut s'en passer.. :D
 //extern FILE * fichier_db;
@@ -226,6 +310,7 @@ extern Mix_Chunk *effect;
 extern int sel_menu_m; //Indice du choix (menu) survolé à un moment t.
 
 extern char buffer; //Where we will keep last char from keyboard !
+
 extern int buffer_deliver;
 
 extern Input in, tmp;
